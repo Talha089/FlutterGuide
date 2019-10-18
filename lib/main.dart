562d53bf32bdbe9b1c75a03766217024974cc999
Talha089 <talhajavaidmalik@gmail.com>
2019-10-18 15:44:24 +0500

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print("Answer Chosen");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var question = [
+      'What\'s your favourite color',
+      'What\'s your favourite animal?'
+    ];
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Jeda Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(question.elementAt(0)),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
